3bd6e35cc187580029ec5e76dd921c4c41af96a0
Nilav <nilavtalukdar06@gmail.com>
2025-05-06 19:16:32 +0530

    minor code changes

--- src/Chat.jsx
+++ src/Chat.jsx
@@ -5,17 +5,11 @@ import { ClipLoader } from "react-spinners";
 
 const Chat = ({ logOut, isLoading, session }) => {
   const [message, setMessage] = useState("");
-  const [isSubmitting, setIsSubmitting] = useState(false);
   const [userOnline, setUserOnline] = useState(0);
-
-  const sendMessage = async (e) => {
-    e.preventDefault();
-  };
-  console.log(session?.user);
 
   return (
     <section className="h-full p-4 relative sm:p-10">
-      <article className="min-h-[600px] max-w-2xl mx-auto my-5 border rounded-lg border-slate-200 shadow-sm">
+      <article className="min-h-[600px] max-w-2xl mx-auto my-5 border flex flex-col justify-between rounded-lg border-slate-200 shadow-sm">
         <div className="w-full flex justify-between p-4 items-center border-b">
           <div className="text-sm text-gray-500 tracking-wide">
             <p>{session?.user?.email || "Guest"}</p>
@@ -27,17 +21,17 @@ const Chat = ({ logOut, isLoading, session }) => {
             </Button>
           </div>
         </div>
+        <div></div>
         <form
-          className="mt-2 p-4 w-full flex justify-center items-center gap-4"
+          className="border-t border-slate-200 p-4 w-full flex justify-center items-center gap-4"
           onSubmit={(e) => sendMessage(e)}
         >
           <Input
             placeholder="Type a message..."
-            className="w-full rounded"
+            className="w-full"
             onChange={(e) => setMessage(e.target.value)}
             value={message}
             required={true}
-            disabled={isSubmitting}
           />
           <Button variant={"outline"} type={"submit"}>
             Send 👋
